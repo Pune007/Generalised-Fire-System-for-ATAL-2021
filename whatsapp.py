d9aeee0a944cad91e88b8816035dd97d84889c42
Sumedh Natu <73115797+Pune007@users.noreply.github.com>
2022-02-27 10:17:49 +0530

    Update whatsapp.py
    Main code added

--- whatsapp.py
+++ whatsapp.py
@@ -1,1 +1,21 @@
+import serial
+import time
+import pywhatkit
+
+serialMain = serial.Serial('COM3', 9600)
+time.sleep(2)
+
+serialData = []
+for i in range(80):
+    lineRead = serialMain.readline()
+
+    if lineRead:
+        decodedSerial = lineRead.decode()
+        serialInt = int(float(decodedSerial))
+
+        if (serialInt != 0):
+            pywhatkit.sendwhatmsg("+919209388529", "FIRE DETECTED, IMMEDIATE ACTION NECESSARY. PLEASE GIVE ATTENTION\n\0", 19, 16)
+            
+        print(serialInt)
+
 
