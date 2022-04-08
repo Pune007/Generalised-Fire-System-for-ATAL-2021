29a9d8db8cf10711e837488a0a0527ce742dbc8e
Sumedh Natu <73115797+Pune007@users.noreply.github.com>
2022-04-08 22:10:48 +0530

    Update whatsapp.py
    Descriptions added

--- whatsapp.py
+++ whatsapp.py
@@ -1,21 +1,38 @@
+# Various Modules are imported which are necessary for the project to function
 import serial
 import time
 import pywhatkit
 
+# A connection is established between the Arduino and the messaging system (In this case the laptop) through the Serial Port the Arduino is connected to
 serialMain = serial.Serial('COM3', 9600)
 time.sleep(2)
 
+# Time values needed for the project is established
+currentTime = time.localtime()
+
+currentHour = currentTime.tm_hour
+currentMinute = currentTime.tm_min
+
+# The time when the message is to be sent is established, being 1 minute after the current time
+extraTime = 1;
+extraMinutes = currentMinute + extraTime
+
+print(currentHour, currentMinute, extraMinutes)
+
+# A function is run through which a message is sent after multiple
 serialData = []
 for i in range(80):
+    # The value printed by the Serial Monitor is read by the Arduino
     lineRead = serialMain.readline()
 
+    # The printed value is converted from a string (text) to an integer
     if lineRead:
         decodedSerial = lineRead.decode()
         serialInt = int(float(decodedSerial))
 
         if (serialInt != 0):
-            pywhatkit.sendwhatmsg("+919209388529", "FIRE DETECTED, IMMEDIATE ACTION NECESSARY. PLEASE GIVE ATTENTION\n\0", 19, 16)
+            # If the serialInt is not 0, meaning a fire is detected; a message is sent
+            pywhatkit.sendwhatmsg("+917249355827", "FIRE DETECTED, IMMEDIATE ACTION NEEDED AT SECTION-3\n\0", currentHour, extraMinutes)
             
         print(serialInt)
-
 
